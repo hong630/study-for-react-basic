b44f3198b48cb57f0b07267e9a1a9f8d92194147
hong630 <hongihongi60@gmail.com>
2022-04-26 23:56:04 +0900

    sleepy..

--- src/App.js
+++ src/App.js
@@ -3,29 +3,28 @@ import ReactDOM from 'react-dom'
 import Button from "./Button"
 import styles from './App.module.css'
 import {useState} from 'react'
+
+
 function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState('');
-    const onClick = () => setValue((prev)=> prev+1);
-    const onChange = (event) => setKeyword(event.target.value);
-    console.log('I run all the time');
-    useEffect(()=>{
-        console.log('CALL THE API...')
-    },[]);
-    useEffect(()=>{
-        if(keyword !== "" && keyword.length > 5){
-            console.log('Search For', keyword)
-        }
-    },[keyword])
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange =(event)=>{
+    setToDo(event.target.value)
+  }
+  const onSubmit = (event) =>{
+    event.preventDefault();
+    if(toDo === ""){
+      return
+    }
+    setToDo("");
+  }
+  console.log(toDos)
   return (
     <div>
-        <input
-            value={keyword}
-            onChange={onChange}
-            type="text"
-            placeholder="search Here"/>
-      <h1 className={styles.title}>{counter}</h1>
-        <button onClick={onClick}>Click Me</button>
+      <form onSubmit={onSubmit} action="">
+        <input onChange={onChange} value={toDo} type="text" placeholder={"write your todo"}/>
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
