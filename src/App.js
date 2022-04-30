5c72efc78cc60683ff1368f1cf15ccc8a9fce5c4
hong630 <hongihongi60@gmail.com>
2022-04-30 20:39:33 +0900

    영화앱만들기 part1

--- src/App.js
+++ src/App.js
@@ -3,44 +3,38 @@ import ReactDOM from 'react-dom'
 import Button from "./Button"
 import styles from './App.module.css'
 import {useState} from 'react'
+import {resetFirstInputPolyfill} from "web-vitals/dist/modules/lib/polyfills/firstInputPolyfill";
 
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () =>{
+    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`);
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false)
+  }
   useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers")
-        .then((response) => response.json())
-        .then((json) => {setCoins(json); setLoading(false)});
+    getMovies()
   },[])
-  const [money, setMoney] = useState(0);
-  const [cost , setCost] = useState(0);
-  const [result , setResult] = useState(0);
-  const onChange = (e) => {
-    setMoney(e.target.value);
-    console.log(money)
-  }
-  const getSymbol = (e) =>{
-    setCost(e.target.value);
-  }
-  const calculate =()=>{
-    setResult(Math.floor(money / cost))
-  }
+  console.log(movies);
   return (
-    <div>
-      <h1>The Coins! ({coins.length})</h1>
-      <input type="text" placeholder='how much do you have?' onChange={onChange}/>
-      <button  onClick={calculate}>calculate!</button>
-      {loading ? <strong>loading..</strong> :
-          (<select id="select" onChange={getSymbol}>
-            {coins.map(
-                (coin)=><option value={coin.quotes.USD.price} key={coin.id}>{coin.name}({coin.symbol}) : {coin.quotes.USD.price} USD</option>)
-            }
-          </select>)
-      }
-      <div>you can get {result}</div>
-    </div>
-  );
+      <div>
+        {loading
+        ? <h1>loading...</h1>
+        : movies.map((movie)=>(
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} alt={movie.title} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map(g=><li key={g}>{g}</li>)}
+              </ul>
+           </div>
+        ))}
+      </div>
+  )
 }
 
 export default App;
