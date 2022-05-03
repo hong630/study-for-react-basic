b1393a36808d9657c509914d4538945484cc8fef
hong630 <hongihongi60@gmail.com>
2022-05-04 00:09:01 +0900

    chanllenge 남았음

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-
+import React, {useEffect} from 'react'
+import {useParams} from "react-router-dom";
 
 function Detail(){
+    const {id} = useParams();
+    const getMovie = async()=>{
+        const json = await(await fetch(`https://yts.mx/api/v2/list_movies.json?movie_id=${id}`)).json();
+        console.log(json);
+    }
+    useEffect(()=>{
+        getMovie();
+    },[])
     return <h1>Detail</h1>
 }
 
